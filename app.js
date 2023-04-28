b68023b5af9c5b513cfebfef836a3d2b78a5973a
Miksam13 <maks1313137022@gmail.com>
2023-04-28 19:19:19 +0200

    [feat]time-zone-calls

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ dotenv.config();
 const { BOT_TOKEN, ADMINS_ID } = process.env;
 const bot = new Telegraf(BOT_TOKEN);
 const chats = JSON.parse(fs.readFileSync("database/db.json", "utf8"));
+const citiesData = fs.readFileSync('database/cities15000.txt', 'utf8');
+const cities = citiesData.split('\n');
 const adminUsersId = [];
+const callUsers = [];
 
 const checkUser = (ctx, next) => adminUsersId.includes(ctx.from.id) ? next() : ctx.reply('У тебя нет прав доступа админа');
 ADMINS_ID.split(", ").forEach((id) => adminUsersId.push(+id));
@@ -17,8 +20,16 @@ let callDay = 'сегодня';
 
 bot.start((ctx) => {
   chats.push(ctx.message.chat);
-  fs.writeFileSync("database/db.json", JSON.stringify(chats));
-  ctx.reply('Hello');
+  const uniqueIds = [];
+  const newChats = []
+  chats.forEach(chat => {
+    if (!uniqueIds.includes(chat.id)) {
+      uniqueIds.push(chat.id);
+      newChats.push(chat);
+    }
+  });
+  fs.writeFileSync("database/db.json", JSON.stringify(newChats));
+  ctx.reply('Hello, this bot...');
 });
 
 bot.hears('/allconnects', checkUser, (ctx) => {
@@ -34,8 +45,8 @@ bot.hears('/allconnects', checkUser, (ctx) => {
 });
 
 // on future)))
-
-/*function getNextWeekdayDate(weekday) {
+/*
+function getNextWeekdayDate(weekday) {
   const weekdaysRegex = /понедельник|вторник|сред[ау]|четверг|пятниц[ау]|суббот[ау]|воскресенье/i;
   if (!weekdaysRegex.test(weekday)) {
     throw new Error("Invalid weekday name");
@@ -66,21 +77,84 @@ bot.hears('/allconnects', checkUser, (ctx) => {
 }
 
 // Пример использования
-const weekday = "субботу";
+const weekday = "вторник";
 const nextWeekdayDate = getNextWeekdayDate(weekday);
 console.log(getNextWeekdayDate(weekday))
-console.log(`Следующий ${weekday} - ${nextWeekdayDate.toLocaleDateString()}`);*/
+console.log(`Следующий ${weekday} - ${nextWeekdayDate.toLocaleDateString()}`);
+*/
 
-bot.on('message', (ctx) => {
-  const regExpValidateTime = /^([01]\d|2[0-3])[:., ]([0-5]\d)$/;
-  const regExpValidateTime2 = /^[0-2][0-4]$/;
-  const regExpTime = /^([01]\d|2[0-9])[:., ]([0-9]\d)$/;
-  const regExpTime2 = /^[0-9][4-9]$/;
+bot.on('poll_answer', (ctx) => {
+  const { id, username, first_name } = ctx.update.poll_answer.user;
+  let option_id = ctx.update.poll_answer.option_ids[0];
+
+  if(option_id === 0) {
+    callUsers.push({id: id, username: username, first_name: first_name})
+  }
+})
+
+bot.hears('/getpollusers', (ctx) => {
+  let str = '';
+
+  if(callUsers.length !== 0) {
+    str = `На совзоне будут:\n`;
+    callUsers.map((user) => {
+      str += ` <a href="https://${user.username}.t.me">${user.first_name}</a>`;
+    })
+  } else {
+    str += 'На созвоне никого не будет))))';
+  }
+  ctx.replyWithHTML(str, { disable_web_page_preview: true });
+})
+
+function findMatch(word, wordsArray) {
+  for (let i = 0; i < wordsArray.length; i++) {
+      const baseWord = wordsArray[i].slice(0, -1);
+        if (baseWord === word || baseWord === word.slice(0, -1)) {
+          return word.slice(0, -1);
+        }
+    }
+  return null;
+}
+
+function findCityName(message) {
+  let city = [];
+  for (let i = 0; i < cities.length; i++) {
+    const cityData = cities[i].split('\t');
+    if (cityData[3] !== '' && cityData[3].includes(',')) {
+        city = cityData[3].split(',');
+    } else {
+      city.push(cityData[3]);
+    }
+    for (let j = 0; j < city.length; j++) {
+      if (findMatch(message.slice(0, -1), city)) {
+        return cityData[17];
+      } else if (city[j].toLowerCase() === message.toLowerCase()) {
+          return cityData[17];
+      }
+    }
+  }
+  return null;
+}
+
+bot.on('message', async (ctx) => {
+  const regExpValidateTime = /^([01]\d|2[0-3])[:., ]([0-5]\d)/;
+  const regExpValidateTime2 = /^([01]?[0-9]|2[0-4])/;
+  const regExpTime = /^([01]\d|[0-2][0-9])[:., ]([0-9]\d)/;
+  const regExpTime2 = /^[0-9][4-9]/;
   const regExpTitleChat = /(?<=чате ).*$/;
+  const regExpCity = /по\s+(.*)/i;
   const regExpTimeMessage = /[ ,.]/g;
   const regExpTomorrow = /завтра/gi;
   const regExpWeekDays = /понедельник|вторник|сред[ау]|четверг|пятниц[ау]|суббот[ау]|воскресенье/gi;
   const text_message = ctx.message.text;
+  const cityMatch = text_message.match(regExpCity);
+  let timeZone;
+  const optionsPool = [
+    'Я буду',
+    'Меня не будет',
+    'Буду позже',
+  ];
+
 
   if (ctx.message.chat.type === "private") {
     if (regExpValidateTime.test(text_message) || regExpValidateTime2.test(text_message) && stateMsg === 'time') {
@@ -88,13 +162,22 @@ bot.on('message', (ctx) => {
       if (regExpValidateTime.test(text_message)) {
         time_message = text_message.replace(regExpTimeMessage, ':');
       } else if (regExpValidateTime2.test(text_message)) {
-        time_message = text_message + ':00';
+        if (text_message === '24') {
+          time_message = '00:00';
+        } else {
+          time_message = text_message + ':00';
+        }
       }
-      bot.telegram.sendMessage(chat.id, `${ctx.message.chat.username} хочет организовать созвон ${callDay} на ${time_message}`);
+      if (cityMatch !== '') {
+        timeZone = findCityName(cityMatch[1]);
+        ctx.reply('Время зафиксировано');
+        stateMsg = 'pool';
+      } else {
+        ctx.reply('Город не найден)))');
+      }
+      await bot.telegram.sendPoll(chat.id, `${ctx.message.chat.username} хочет организовать созвон ${callDay} на ${time_message} по времени ${timeZone}`, optionsPool, { is_anonymous: false });
       callDay = 'сегодня';
       chat = {};
-      ctx.reply('Время зафиксировано');
-      stateMsg = 'pool';
     } else if(regExpTime.test(text_message) || regExpTime2.test(text_message)) {
       stateMsg = 'time';
       ctx.reply('Уууупс, такого времени еще не придумали))))');
@@ -108,7 +191,7 @@ bot.on('message', (ctx) => {
           callDay = 'в ' + text_message.match(regExpWeekDays)[0]
         }
         stateMsg = 'time';
-        ctx.reply('Во сколько ты хочешь организовать созвон?(формат ввода: XX.XX, XX,XX, XX:XX, XX XX или XX)');
+        ctx.reply('Во сколько ты хочешь организовать созвон?(формат ввода: XX.XX, XX,XX, XX:XX, XX XX или XX по (город), например(по Киеву)');
       } else {
         stateMsg = 'pool';
         ctx.reply('Такого чата в базе не найдено!!!');
